b8921e7b0cbb8e2ea6dfbec7acf6ec3bdb348651
Ameer <ameerapex496@gmail.com>
2023-12-01 20:11:01 +0100

    added effect functions to the button

--- src/script.js
+++ src/script.js
@@ -1,8 +1,29 @@
 // selecting our elements
 var effectButton = document.getElementsByClassName("effect-Button")[0];
 // our functions
-var fireEffect = function () {
-    console.log("start of project");
+var fireEffect = function (e) {
+    // creating our element dynamically
+    var circleEl = document.createElement("div");
+    circleEl.classList.add("circle");
+    // getting information from our e target
+    // getting client click x and y
+    var x = e.clientX;
+    var y = e.clientY;
+    // getting our offset targets
+    var buttonTop = e.target.offsetTop;
+    var buttonLeft = e.target.offsetLeft;
+    // calculating
+    var xInside = x - buttonLeft;
+    var yInside = y - buttonTop;
+    console.log("this is target: ", e);
+    circleEl.style.top = "".concat(yInside, "px");
+    circleEl.style.left = "".concat(xInside, "px");
+    // setting timeout function
+    setTimeout(function () { return circleEl.remove(); }, 500);
+    effectButton.appendChild(circleEl);
 };
 // our eventlisnters
-effectButton.addEventListener("click", fireEffect);
+effectButton.addEventListener("click", function (e) {
+    // sending our target info to fire effect function
+    fireEffect(e);
+});
--- src/script.ts
+++ src/script.ts
@@ -5,9 +5,36 @@ const effectButton = document.getElementsByClassName(
 
 // our functions
 
-const fireEffect = (): void => {
-  console.log("start of project");
+const fireEffect = (e): void => {
+  // creating our element dynamically
+  const circleEl = document.createElement("div");
+  circleEl.classList.add("circle");
+
+  // getting information from our e target
+
+  // getting client click x and y
+  const x = e.clientX;
+  const y = e.clientY;
+
+  // getting our offset targets
+  const buttonTop = e.target.offsetTop;
+  const buttonLeft = e.target.offsetLeft;
+
+  // calculating
+  const xInside = x - buttonLeft;
+  const yInside = y - buttonTop;
+  console.log("this is target: ", e);
+  circleEl.style.top = `${yInside}px`;
+  circleEl.style.left = `${xInside}px`;
+
+  // setting timeout function
+  setTimeout(() => circleEl.remove(), 500);
+
+  effectButton.appendChild(circleEl);
 };
 
 // our eventlisnters
-effectButton.addEventListener("click", fireEffect);
+effectButton.addEventListener("click", (e) => {
+  // sending our target info to fire effect function
+  fireEffect(e);
+});
